be4022d259bf308fa662165b3a43213deea98551
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-04-01 06:00:51 +0500

    fixed build

--- src/pages/components/Views/Blog/Header.tsx
+++ src/pages/components/Views/Blog/Header.tsx
@@ -57,7 +57,7 @@ const Header = () => {
                                 {trendingPost.map((item, index) => {
                                     return (
                                         <div key={index}>
-                                            <Card image={item.img} title={item.title} buttonTitle={item.btnTitle} seccolor={''} fontweight={'600'} display={''} visible={''} gap={''} alignItems={''} title2={''} secFontweight={''} secFontSize={''} show={''} imgMaxHeight={''} width={''} outerImg={''} minHeight={''} />
+                                            <Card image={item.img} title={item.title} buttonTitle={item.btnTitle} seccolor={''} fontweight={'600'} display={''} visible={''} gap={''} alignItems={''} title2={''} secFontweight={''} secFontSize={''} show={''} imgMaxHeight={''} width={''} outerImg={''} minHeight={''} speed={undefined} />
                                         </div>
                                     )
                                 })}
